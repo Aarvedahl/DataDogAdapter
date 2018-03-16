d1d4cc084557f38dbe81fb4e25379502ab6c79bd
Alex Arvedahl <alex.arvedahl@gmail.com>
2018-03-16 08:54:36 +0100

    Restore user

--- src/main/java/alex/tdi/test/AdapterTest.java
+++ src/main/java/alex/tdi/test/AdapterTest.java
@@ -17,7 +17,8 @@ public class AdapterTest {
        // testGet(adapter2);
        // testAdd(adapter2);
        // testUpdate(adapter2);
-      //  testDisable(adapter2);
+       // testDisable(adapter2);
+        testRestore(adapter2);
 
 
     }
@@ -90,90 +91,5 @@ public class AdapterTest {
         System.out.println("Updated name of the user:" + resultDTO.getResponseDTO().user.name);
         System.out.println("User disabled:" + resultDTO.getResponseDTO().user.disabled);
     }
-
-     /*
-    private static void testAddAccount(Adapter client) {
-        AccountDTO account = new AccountDTO();
-        account.handle = "alex.a2@enfo.org";
-        account.name = "Alex A2";
-        account.access_role = "st";
-        account.email = "alex.a2@enfo.org";
-        account.disabled = false;
-
-        String url = "https://app.datadoghq.com/api/v1/user";
-        ResultDTO result = client.addAccount(account, url, api_key, app_key);
-        ResponseDTO respobj = (ResponseDTO) result.getResponseDTO();
-        System.out.println("New handle=" + respobj.user.handle);
-        System.out.println("Name of the user =" + respobj.user.name);
-    }
-
-
-    private static void testGetAccount(Adapter client) {
-        AccountDTO account = new AccountDTO();
-        account.handle = "alex.a1@enfo.org";
-        account.name = "Alex A1";
-        account.access_role = "st";
-        account.email = "alex.a1@enfo.org";
-        account.disabled = false;
-
-        String dogUrl = "http://app.datadoghq.com/api/v1/user/";
-        ResultDTO resultDTO = client.getAccount(account, dogUrl, api_key, app_key);
-        System.out.println("Get user result=" + resultDTO.getResultcode());
-        ResponseDTO respobj = (ResponseDTO) resultDTO.getResponseDTO();
-        System.out.println("handle of the user=" + respobj.user.handle);
-        System.out.println("Email of the user=" + respobj.user.email);
-    }
-
-    private static void testGetAccount(Adapter client) {
-        AccountDTO account = new AccountDTO();
-        account.handle = "alex.a1@enfo.org";
-        account.name = "Alex A1";
-        account.access_role = "st";
-        account.email = "alex.a1@enfo.org";
-        account.disabled = false;
-
-        ResultDTO resultDTO = client.getAccount(account, "https://app.datadoghq.com/api/v1/user/", "4", "3", api_key, app_key);
-        System.out.println("get user result=" + resultDTO.getResultcode());
-        ResponseDTO respobj = (ResponseDTO) resultDTO.getResponseDTO();
-        System.out.println("handle of the user=" + respobj.user.handle);
-        System.out.println("Email of the user=" + respobj.user.email);
-        System.out.println("result JSON=" + resultDTO.getResultJSON());
-    }
-
-
-    private static void testModifyAccount(Adapter client) {
-        AccountDTO accountDTO = new AccountDTO();
-        accountDTO.handle = "test@test.com";
-        accountDTO.name = "Testie Test";
-        accountDTO.email = "test@test.com";
-        accountDTO.disabled = false;
-        accountDTO.access_role = "ro";
-
-        String url = "https://app.datadoghq.com/api/v1/user/";
-        ResultDTO resultDTO = client.changeAccount(accountDTO, url, "4", "3", api_key, app_key);
-        System.out.println("Modify user result=" + resultDTO.getResultcode());
-        ResponseDTO respobj = (ResponseDTO) resultDTO.getResponseDTO();
-        System.out.println("handle of the user=" + respobj.user.handle);
-        System.out.println("Name of the user =" + respobj.user.name);
-        System.out.println("Result JSON=" + resultDTO.getResultJSON());
-    }
-
-
-    private static void testDisableAccount(Adapter client) {
-        AccountDTO accountDTO = new AccountDTO();
-        accountDTO.handle = "test@test.com";
-        accountDTO.name = "Testie Test";
-        accountDTO.email = "test@test.com";
-        accountDTO.disabled = true;
-        accountDTO.access_role = "ro";
-
-        String url = "https://app.datadoghq.com/api/v1/user/";
-        ResultDTO resultDTO = client.disableAccount(accountDTO, url, "4", "3", api_key, app_key);
-        System.out.println("Disable user result=" + resultDTO.getResultcode());
-        ResponseDTO responseDTO = (ResponseDTO) resultDTO.getResponseDTO();
-        System.out.println("Response Message=" + responseDTO.message);
-        System.out.println("Result JSON=" + resultDTO.getResultJSON());
-    }
- */
-
+    
 }
